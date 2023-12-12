02f6ed9a71ce97a5263ad728dcd4a5b17a118eaa
GagarinIstu <146407863+GagarinIstu@users.noreply.github.com>
2023-12-13 02:14:32 +0800

    Add files via upload

---
+++ task_M02.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+/*Для нахождения площади прямоугольника по формуле S = n*a^2/(4*tg(pi/n)),
+где а-длина сторона многоугольника, n-число сторон мне понадобился тангес и для этого я поключил библиотеку сmath*/
+
+float get_square(float A, int N)
+{
+    float square=(N*A*A)/(4*tan(M_PI/N));
+    return square;
+}
+int main()
+{
+    setlocale(0, "");
+    float A;
+    int N;
+    cout << "Enter the side length А: ";
+    cin >> A;
+    cout << "Enter the number of sides N: ";
+    cin >> N;
+    float square = get_square(A, N);
+    cout << "The area of a polygon with side length A and number of sides N is equal to " << square << endl;
+    return 0;
+
+}
